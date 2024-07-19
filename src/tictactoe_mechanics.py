f9d4acef49c0d52e902de84eca4075f1b017f306
FDijkema <fmdijkema@gmail.com>
2024-07-20 00:07:25 +0200

    working basic gameplay

--- src/tictactoe_mechanics.py
+++ src/tictactoe_mechanics.py
@@ -4,12 +4,18 @@ import numpy as np
 class TictactoeGame:
     def __init__(self):
         self.state = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
-        self.player_name = ["Player 1", "Player 2"]
+        self.player_name = {
+            1: "Player 1",
+            -1: "Player 2"
+        }
         self.next_to_move = 1    # 1 for player 1, -1 for player 2
         self.winner = None
         self.game_over = False
 
     def move(self, row, column):
+        # convert to 0-base indexing
+        row = row - 1
+        column = column - 1
         if self.game_over:    # can't make a move after the game ended
             print("This game is already decided. You can't play anymore.")
         elif self.state[row, column] == 0:    # check if the field is empty
@@ -22,11 +28,26 @@ class TictactoeGame:
             print("This move is invalid.")
 
     def is_winning_move(self, row, column):
-        # winning on row todo
-        # winning on column todo
-        # winning on diagonal todo
-        # winning by no more moves to make todo
-        self.game_over = True
-        return True
-
-
+        row_sum = abs(np.sum(self.state[row, :]))
+        column_sum = np.abs(np.sum(self.state[:, column]))
+        diag_sum_left = np.abs(np.sum(np.diag(self.state)))
+        diag_sum_right = np.abs(np.sum(np.diag(np.fliplr(self.state.copy()))))
+        winning_configurations = (
+            row_sum == 3,
+            column_sum == 3,
+            diag_sum_left == 3,
+            diag_sum_right == 3
+        )
+        # close_calls = (
+        #    row_sum == 2,
+        #    column_sum == 2,
+        #    diag_sum_left == 2,
+        #    diag_sum_right == 2)
+        if any(winning_configurations):
+            self.game_over = True
+            return True
+        # elif sum(close_calls) >= 2:    # win in the next round guaranteed
+        #    self.game_over = True
+        #    return True    # also signal that the game has been won already
+        else:
+            return False
