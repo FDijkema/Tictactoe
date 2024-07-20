1790081bbc427e767fa9c6d5d2b3e2a8e1fe1455
FDijkema <fmdijkema@gmail.com>
2024-07-20 09:51:24 +0200

    Slight improvement in message for invalid move

--- src/tictactoe_mechanics.py
+++ src/tictactoe_mechanics.py
@@ -25,7 +25,7 @@ class TictactoeGame:
                 print("Congratulations! {} wins!".format(self.winner))
             self.next_to_move *= -1    # advance the game to the next move
         else:   # can't make a move on filled field
-            print("This move is invalid.")
+            print("This move is invalid. Try an empty field instead.")
 
     def is_winning_move(self, row, column):
         row_sum = abs(np.sum(self.state[row, :]))
