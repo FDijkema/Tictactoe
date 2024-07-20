44ba2b4a9725dc9c5de3fc634266bb44a1ff3a7b
FDijkema <fmdijkema@gmail.com>
2024-07-20 14:54:21 +0200

    Background with game board created

--- src/main.py
+++ src/main.py
@@ -8,13 +8,29 @@ def new_game():
     # fill the screen
     screen.fill("wheat3")
     print("new game started")
+
     # screen layout
-    text_box = pygame.Rect(width, 100, 0,0)
     pygame.draw.rect(screen, "wheat4", pygame.Rect(0, 0, width, 100))
+    pygame.draw.line(screen, "black", (0, 100), (width, 100), 5)
+    # board
+    pygame.draw.line(screen, "wheat4",
+                     (margin, top_of_board + square_size),
+                     (width - margin, top_of_board + square_size), 10)
+    pygame.draw.line(screen, "wheat4",
+                     (margin, top_of_board + 2 * square_size),
+                     (width - margin, top_of_board + 2 * square_size), 10)
+    pygame.draw.line(screen, "wheat4",
+                     (square_size, top_of_board + margin),
+                     (square_size, height - margin), 10)
+    pygame.draw.line(screen, "wheat4",
+                     (2 * square_size, top_of_board + margin),
+                     (2 * square_size, height - margin), 10)
+
+
     # write a welcome text
     font = pygame.font.Font('freesansbold.ttf', 32)
     text = font.render('Welcome to Tic-tac-toe', True, "black")
-    text_rect = text.get_rect(midtop=(width / 2, margin))
+    text_rect = text.get_rect(center=(width / 2, 50))
     screen.blit(text, text_rect)
     # create boxes to make a board
 
@@ -31,6 +47,8 @@ pygame.event.set_allowed(pygame.MOUSEBUTTONDOWN)
 width = 600
 height = 700
 margin = 10
+top_of_board = 100
+square_size = 200
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Tic-Tac-Toe')
 new_game()
