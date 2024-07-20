4a596b1a6237a13c06cbf69e3f664d9027d6532a
FDijkema <fmdijkema@gmail.com>
2024-07-20 11:51:48 +0200

    Docstrings for game mechanics

--- src/tictactoe_mechanics.py
+++ src/tictactoe_mechanics.py
@@ -1,8 +1,47 @@
+"""This module takes care of the game mechanics of tictactoe, so you can focus on the UI"""
+
 import numpy as np
 
 
 class TictactoeGame:
+    """
+        A class performing the mechanics of a game of Tictactoe.
+
+        ...
+
+        Attributes
+        ----------
+        state : numpy.ndarray
+            keeps track of the state of the game board
+            filled with 0 at the beginning of the game
+            1 represents a move by player 1
+            -1 represents a move by player 2
+        self.player_name : dict
+            links the name of the player (player 1 or player 2) to the symbol (1 or -1)
+        next_to_move : int
+            what symbol is used by the player who will make the next move
+            can be 1 or -1
+        winner : str
+            who wins the game?
+            returns None while the game is undecided
+            Possible values after Game over: "Player 1", "Player 2" or "Draw"
+        game_over : bool
+            whether the game is decided or not
+        number_of_moves : int
+            number of moves made in the game so far
+
+        Methods
+        -------
+        __init__():
+            initializes the game board
+        move(row: int, column: int):
+            advances the game by placing the symbol of the player who is next to move on the board
+            at the indicated row and column
+            row and column index starts from 1
+            updates class attributes accordingly if the game has ended in win or draw
+        """
     def __init__(self):
+        """initiates a new game board"""
         self.state = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
         self.player_name = {
             1: "Player 1",
@@ -14,6 +53,16 @@ class TictactoeGame:
         self.number_of_moves = 0
 
     def move(self, row, column):
+        """
+        advances the game by placing the symbol of the player who is next to move on the board
+
+        Parameters
+        ----------
+        row: int
+            row index where the player will move (base 1)
+        column: int
+            column index where the player will move (base 1)
+        """
         # convert to 0-base indexing
         row = row - 1
         column = column - 1
@@ -23,7 +72,7 @@ class TictactoeGame:
             self.state[row, column] = self.next_to_move    # place player symbol in the right field
             self.number_of_moves += 1
             # evaluate the result of the move
-            if self.is_winning_move(row, column):    # game ends by win
+            if self._is_winning_move(row, column):    # game ends by win
                 self.game_over = True
                 self.winner = self.player_name[self.next_to_move]
                 print("Congratulations! {} wins!".format(self.winner))
@@ -37,7 +86,10 @@ class TictactoeGame:
         else:   # can't make a move on filled field
             print("This move is invalid. Try an empty field instead.")
 
-    def is_winning_move(self, row, column):
+    def _is_winning_move(self, row, column):
+        """returns a boolean value indicating whether the game has ended in a win
+            the check is made based on the game_state, row and column are only passed to check
+            the board more efficiently"""
         row_sum = abs(np.sum(self.state[row, :]))
         column_sum = np.abs(np.sum(self.state[:, column]))
         diag_sum_left = np.abs(np.sum(np.diag(self.state)))
@@ -56,7 +108,6 @@ class TictactoeGame:
         if any(winning_configurations):
             return True
         # elif sum(close_calls) >= 2:    # win in the next round guaranteed
-        #    self.game_over = True
         #    return True    # also signal that the game has been won already
         else:
             return False
