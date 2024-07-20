a71e34898f3c95fa0a9bf35f483ba72ac31e42f5
FDijkema <fmdijkema@gmail.com>
2024-07-20 13:04:06 +0200

    Initial game loop with very basic functionality. Screen is created and can be closed. New game function is triggered. Mouseclick is registered and triggers function to make a move.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,36 @@
+# Example file showing a circle moving on screen
+import sys
 
+import pygame
+
+
+def new_game():
+    # fill the screen
+    screen.fill("white")
+    print("new game started")
+    # write a welcome text
+    # create boxes to make a board
+
+def make_a_move():
+    print("move made")
+    # pygame.Rect.collidepoint
+
+# pygame setup
+pygame.init()
+# global settings for pygame
+pygame.event.set_allowed(None)
+pygame.event.set_allowed(pygame.MOUSEBUTTONDOWN)
+
+screen = pygame.display.set_mode((600, 600))
+pygame.display.set_caption('Tic-Tac-Toe')
+new_game()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:    # user closed window and quit game
+            pygame.quit()
+            quit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:    # used clicked on something in the window
+            pos = pygame.mouse.get_pos()
+            # make a move
+            make_a_move()
